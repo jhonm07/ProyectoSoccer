7651c4ecc49f8c9f0e76a7cd9135cfe2f9769fc5
jhon <jhon@jhon-Q470C-Q468C-500P4C>
2016-10-27 21:58:25 -0500

    Se agrega clase GameUtils, la cual esta contenida en otro paquete, se avanza en la guia hasta el punto 8.3

--- src/soccer/Game.java
+++ src/soccer/Game.java
@@ -6,6 +6,8 @@
 
 package soccer;
 
+import utility.GameUtils;
+
 /**
  *
  * @author Administrator
@@ -16,4 +18,24 @@ public class Game {
     public Team awayTeam;
     public Goal[] goals;
     
+    
+    
+
+public void playGame(){
+int numberOfGoals = (int)(Math.random() * 7);
+        Goal[] theGoals = new Goal[numberOfGoals];
+        this.goals = theGoals;
+        GameUtils.addGameGoals(this); 
+}
+public String getDescription(){
+    StringBuilder returnString = new StringBuilder();
+        for (Goal currGoal: this.goals) {
+            returnString.append("Goal scored after "
+            + currGoal.theTime + " mins by "
+            + currGoal.thePlayer.playerName + " of "
+            + currGoal.theTeam.teamName +
+              "\n");
+        }
+        return returnString.toString();
+}
 }
--- src/soccer/League.java
+++ src/soccer/League.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package soccer;
+
+import utility.GameUtils;
 
 /**
  *
@@ -16,20 +17,36 @@ public class League {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        Team[] theTeams = createTeams();
+        Game[] theGames = createGames(theTeams);
+
+        Game currGame = theGames[0];
         
-        // Create team1      
+        currGame.playGame();
+        
+        System.out.println(currGame.getDescription());
+        //System.out.println(numberOfGoals    );
+                
+        
+        
+
+    }
+
+    public static Team[] createTeams() {
+
         Player player1 = new Player();
         player1.playerName = "George Eliot";
         Player player2 = new Player();
         player2.playerName = "Graham Greene";
         Player player3 = new Player();
         player3.playerName = "Geoffrey Chaucer";
-        Player[] thePlayers = {player1, player2, player3 };
-        
+        Player[] thePlayers = {player1, player2, player3};
+
         Team team1 = new Team();
         team1.teamName = "The Greens";
         team1.playerArray = thePlayers;
-        
+
         // Create team2
         Team team2 = new Team();
         team2.teamName = "The Reds";
@@ -40,25 +57,17 @@ public class League {
         team2.playerArray[1].playerName = "Robbie Burns";
         team2.playerArray[2] = new Player();
         team2.playerArray[2].playerName = "Rafael Sabatini";
-        
-        Game currGame = new Game();
-        currGame.homeTeam = team1;
-        currGame.awayTeam = team2;
-        Goal goal1 = new Goal();
-        goal1.thePlayer = currGame.homeTeam.playerArray[2];
-        goal1.theTeam = currGame.homeTeam;
-        goal1.theTime = 55;
-        Goal[] theGoals = {goal1};
-        currGame.goals = theGoals;
-        
-        System.out.println("Goal scored after " + 
-                currGame.goals[0].theTime + " mins by " +
-                currGame.goals[0].thePlayer.playerName + " of " +
-                currGame.goals[0].theTeam.teamName);
-        
-        /* Practice 7-1. Add code for finding a player within team2 here */
-        
-        
-        
-    }   
+
+        Team[] theTeams = {team1, team2};
+        return theTeams;
+    }
+
+    public static Game[] createGames(Team[] theTeams) {
+        Game theGame = new Game();
+        theGame.homeTeam = theTeams[0];
+        theGame.awayTeam = theTeams[1];
+        Game[] theGames = {theGame};
+        return theGames;
+    }
+
 }
